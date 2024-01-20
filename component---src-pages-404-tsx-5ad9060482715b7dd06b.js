b6e232f37970c089ba8e2d7c2ed501907cd039f2
nziranziza <nziranzizadaniel@gmail.com>
2024-01-20 16:27:49 +0200

    2024 update 1

---
+++ component---src-pages-404-tsx-5ad9060482715b7dd06b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknziranziza=self.webpackChunknziranziza||[]).push([[218],{2513:function(n,a,e){e.r(a);var u=e(7294);a.default=()=>u.createElement("main",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-5ad9060482715b7dd06b.js.map
